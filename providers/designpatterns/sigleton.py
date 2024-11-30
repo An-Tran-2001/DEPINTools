8d44ca30f9509b306ff8e4fe6217cd6273ebf374
antv <antv@tinhvan.com>
2024-11-30 23:17:12 +0700

    change base v1 for user dev v1

---
+++ providers/designpatterns/sigleton.py
@@ -0,0 +1,29 @@
+"""
+Copyright © 2024 [tranandeveloper@gmail.com]
+All Rights Reserved.
+
+Licensed under the MIT License. You may obtain a copy of the License at:
+    https://opensource.org/licenses/MIT
+
+Author: TranAn
+"""
+
+
+class SingletonMeta(type):
+    """
+    The Singleton class can be implemented in different ways in Python. Some
+    possible methods include: base class, decorator, metaclass. We will use the
+    metaclass because it is best suited for this purpose.
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
